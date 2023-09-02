5eb7d905d1cab4dbcafb0f64b1653f6f5f5298e8
adriano150 <115594802+adriano150@users.noreply.github.com>
2023-09-01 21:55:42 -0300

    Add files via upload

--- Java-Core/src/aplication/Start_Pedidos.java
+++ Java-Core/src/aplication/Start_Pedidos.java
@@ -13,6 +13,14 @@ public class Start_Pedidos {
 		Pedido pedido1 = new Pedido(543, new Date(), StatusPedidos.PENDENTE_DE_PAGAMENTO);
 		System.out.println(pedido1);
 
+		//Converter um objeto String em enum
+		//Instância de pedido2, receber um status de entregue
+		StatusPedidos pedido2 = StatusPedidos.ENTREGUE;
+		
+		StatusPedidos pedido3 = StatusPedidos.valueOf("ENTREGANDO");
+		
+		System.out.println(pedido2);
+		System.out.println(pedido3);
 	}
 
 }
